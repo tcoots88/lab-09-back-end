dc31595d425ba7f0f26fc68543f71157139d7336
tcoots88 <travis.coots@yahoo.com>
2019-12-13 13:53:18 -0800

    changes

--- modules/yelp.js
+++ modules/yelp.js
@@ -10,8 +10,7 @@ function Yelp(name, image_url, price, rating, url) {
   this.rating = rating;
   this.url = url;
 }
-
-
+console.log("test");
 const getReviews = function(request, response) {
   const url = `https://api.yelp.com/v3/businesses/search?latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
   superagent.get(url).set('Authorization', `Bearer ${process.env.YELP_API_KEY}`).then(data => {
